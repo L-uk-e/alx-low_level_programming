b603808b578a7ce9de2272c22aec1fde74a43b49
Luke Mwangi <lukamwangi.k@gmail.com>
2022-09-10 14:09:03 +0300

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Uses printf to output some random text
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
