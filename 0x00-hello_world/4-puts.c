939876fa8ad3bbd4f4342867cd955ec29fbe1713
Luke Mwangi <lukamwangi.k@gmail.com>
2022-09-10 13:50:43 +0300

    cor 4 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Outputs text
+ * main - Entry point
+ *
+ * Description: Outputs text using the puts function
+ *
+ * Return: Will always return 0
  */
 int main(void)
 {
