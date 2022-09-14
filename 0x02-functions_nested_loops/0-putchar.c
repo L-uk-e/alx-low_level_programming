48159cc8cd813778b175a7252ec0a08ec476ff95
Luke Mwangi <lukamwangi.k@gmail.com>
2022-09-14 14:06:00 +0300

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('.');
+	putchar('c');
+	putchar('\n');
+
+	return (0);
+}
