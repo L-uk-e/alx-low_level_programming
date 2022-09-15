972535af413f3fea04088e468cb336f33809b79f
Luke Mwangi <lukamwangi.k@gmail.com>
2022-09-15 14:24:18 +0300

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char letter[10] = "_putchar";
 
+	int x = 0;
+
+	while ( x < 8 )
+	{
+		_putchar(letter[x]);
+		x++;
+	}
+	_putchar(10);
 	return (0);
 }
