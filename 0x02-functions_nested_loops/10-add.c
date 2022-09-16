34b95f38fba426525cec56aca8622e7e44e4eb6a
Luke Mwangi <lukamwangi.k@gmail.com>
2022-09-16 17:41:12 +0300

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -8,22 +8,6 @@
 int add(int x, int y)
 {
 	int result = x + y;
-
-	if (result > 99)
-	{
-		_putchar(result / 100 + '0');
-		_putchar(result / 10 + '0');
-		_putchar(result % 10 + '0');
-	}
-	else if (result > 9)
-	{
-		_putchar(result / 10 + '0');
-		_putchar(result % 10 + '0');
-	}
-	else if ((result > 0) && (result < 10))
-	{
-		_putchar(result + '0');
-	}
 		
 	return (result);
 }
