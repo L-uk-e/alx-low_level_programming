c232c7c2d32b2da6d6adb1eea6ac6312a64a3477
Luke Mwangi <lukamwangi.k@gmail.com>
2022-09-16 08:25:27 +0300

    Task 2

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "_putchar.c"
+#include "2-print_alphabet_x10.c"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
+}
