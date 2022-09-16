b2db4e7c20dfb3c5efa3f721f30016861093c753
Luke Mwangi <lukamwangi.k@gmail.com>
2022-09-16 07:54:37 +0300

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,2 +1,18 @@
 #include "main.h"
 
+/**
+ * print_alphabet - Prints alphabets
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "1-alphabet.c"
+#include "_putchar.c"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet();
+    
+    return (0);
+}
