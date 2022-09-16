b66fbcfc8be74da2ae60b9bfa6fb561a04a22be3
Luke Mwangi <lukamwangi.k@gmail.com>
2022-09-16 10:46:47 +0300

    Task 7

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "7-print_last_digit.c"
+#include "_putchar.c"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = print_last_digit(98);
+    _putchar('0' + r);
+    _putchar('\n');
+    return (0);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @n: number whose last digit is to be printed
+ *
+ * Return: Returns value of last digit
+ */
+int print_last_digit(int n)
+{
+	int x = (n % 10);
+
+	return (x);
+}
