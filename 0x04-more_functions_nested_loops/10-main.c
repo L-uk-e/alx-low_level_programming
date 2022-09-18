1ae99246ae08a4240f0af9973d39f9770c7ae630
Luke Mwangi <lukamwangi.k@gmail.com>
2022-09-18 09:35:23 +0300

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "_putchar.c"
+#include "10-print_triangle.c"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_triangle(10);
+    
+    return (0);
+}
