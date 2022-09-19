2e384b383888ff36eb9abb17719d514f9ec7b2ac
Luke Mwangi <lukamwangi.k@gmail.com>
2022-09-19 19:24:44 +0300

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - Computes the length of a string
+ * @s: String given
+ *
+ * Return: Length of given string
+ */
+int _strlen(char *s)
+{
+	int i = 1;
+	int x = 0;
+
+	while (i != 0)
+	{
+		i = s[x];
+		x++;
+	}
+
+	return (x);
+}
