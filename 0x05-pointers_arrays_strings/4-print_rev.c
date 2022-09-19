c8c78c0e48a151463ac5851e5286574e32f6c6f3
Luke Mwangi <lukamwangi.k@gmail.com>
2022-09-19 20:10:52 +0300

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: The string entered
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int x = 10, y = 0, z;
+
+	while (x != 0)
+	{
+		x = s[y];
+
+		y++;
+	}
+
+	z = y - 1;
+
+	while (z >= 0)
+	{
+		_putchar(s[z]);
+
+		z--;
+	}
+
+	_putchar('\n');
+
+}
