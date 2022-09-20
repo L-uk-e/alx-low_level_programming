191eb0efa252c4340401cb13061c4fde185c4561
Luke Mwangi <lukamwangi.k@gmail.com>
2022-09-20 21:32:50 +0300

    Mod 1 Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,41 +5,19 @@
  * @src: String to be copied
  * @dest: where string is to be copied to.
  *
- * Return: Pointer to dest
+ * Return: dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	int l, x = 0;
-
-	l = _strlen(src) - 1;
-
-	while (x <= l)
-	{
-		*(src + x) = *(dest + x);
-		x++;
-	}
-
-	*(src + (x + 1)) = '\0';
-
-	return (dest);
-}
-
-/**
- * _strlen - Computes the length of a string
- * @s: String given
- *
- * Return: Length of given string
- */
-int _strlen(char *s)
-{
-	int i = 1;
 	int x = 0;
 
-	while (i != 0)
+	while (*(src + x) != '\0')
 	{
-		i = s[x];
+		*(dest + x) = *(src + x);
 		x++;
 	}
 
-	return (x - 1);
+	*(dest + x) = '\0';
+
+	return (dest);
 }
