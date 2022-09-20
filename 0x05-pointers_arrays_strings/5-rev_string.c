310a774895e2cd96a2d0225c3ee0cdca7d895208
Luke Mwangi <lukamwangi.k@gmail.com>
2022-09-20 19:55:57 +0300

    Mod 100 Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * rev_string - Reverses a string
@@ -8,34 +9,51 @@
  */
 void rev_string(char *s)
 {
-	int a = 10, b = 0, c = 0, e = 4, f, g;
+	int i, x = 0;
 
-	while (a != 0)
+	i = _strlen(s) - 1;
+
+	while (i > x)
 	{
-		a = *(s + b);
-		b++;
+		swap_char(s + i, s + x);
+		i--;
+		x++;
+	}
+	
+}
+
+/**
+ * _strlen - Computes the length of a string
+ * @s: String given
+ *
+ * Return: Length of given string
+ */
+int _strlen(char *s)
+{
+	int i = 1;
+	int x = 0;
+
+	while (i != 0)
+	{
+		i = s[x];
+		x++;
 	}
 
-	char d[];
+	return (x - 1);
+}
 
-	while (c < b)
-	{
-		*(d + c) = *(s + c);
+/**
+ * swap_char - Swaps the values of two chars
+ * @a: value of one integer
+ * @b: value of the other integer
+ *
+ * Return: void
+ */
+void swap_char(char *a, char *b)
+{
+	char c = *a;
 
-		c++;
-	}
+	*a = *b;
 
-	f = 0;
-	b = b - 2;
-	g = b;
-
-	while (f <= b)
-	{
-		*(s + f) = *(d + g);
-		f++;
-		g--;
-	}
-
-	*(s + (f + 1)) = 0;
-
+	*b = c;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -15,4 +15,6 @@ void print_rev(char *s);
 
 void rev_string(char *s);
 
+void swap_char(char *a, char *b);
+
 #endif
