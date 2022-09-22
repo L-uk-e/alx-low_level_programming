9f50d729a1eed55df80d97ee986e6eaae36dd99c
Luke Mwangi <lukamwangi.k@gmail.com>
2022-09-22 16:23:23 +0300

    Mod 0 Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,14 +9,18 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
 	for (i = 0; i < n && *(src + i) != '\0'; i++)
 	{
 		*(dest + i) = *(src + i);
 	}
 
-	
+	for ( ; i < n; *(dest + i))
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
 
 	return (dest);
 }
