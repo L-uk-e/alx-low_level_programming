fa2d4dd5cd02e413ac86e386d583eaef1a0d43ab
Luke Mwangi <lukamwangi.k@gmail.com>
2022-09-22 17:32:18 +0300

    Mod 4 Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0, x = 0;
+	int i = 0, x = 0, y = 0;
 
+	/* Get number of bytes in i */
 	while (*(dest + i) != '\0')
 	{
 		i++;
 	}
 
+	/* Case when dest is more than incoming src */
 	if (i > n)
 	{
 		while (x < n && *(src + x) != '\0')
@@ -27,20 +29,22 @@ char *_strncpy(char *dest, char *src, int n)
 
 	}
 
+	/* Case when dest is less than or equal to incoming src */
 	else
 	{
-		while (x < n)
+		while ((x < n) && (*(src + x) != '\0'))
 		{
 			*(dest + x) = *(src + x);
 			x++;
 		}
 
-		while (x < i)
+		while (x < n)
 		{
 			*(dest + x) = '\0';
 			x++;
 		}
 	}
+	
 
 	return (dest);
 }
