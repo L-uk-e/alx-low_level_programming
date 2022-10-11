44a0acfb5d06270d266a728b318088b1796c09cd
Luke Mwangi <lukamwangi.k@gmail.com>
2022-10-11 12:45:09 +0300

    Mod 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-struct dog{
+/**
+ * struct dog - new structure data type
+ * @name: pointer to name of dog
+ * @age: age of dog
+ * @owner: pointer to name of owner
+ *
+ * Description: simply that
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
