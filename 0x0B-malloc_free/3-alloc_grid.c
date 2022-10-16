05ffdf423c9d57a62e7029af779a2b060f6c0b1d
Luke Mwangi <lukamwangi.k@gmail.com>
2022-10-16 11:55:26 +0300

    Mod 4

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	p = malloc(sizeof (int *) * height);
+	p = malloc(sizeof(int *) * height);
 
 		if (p == NULL)
 		{
@@ -28,6 +28,12 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		p[i] = malloc(sizeof(int) * width);
+
+		if (p[i] == NULL)
+		{
+			free(p);
+			return (NULL);
+		}
 	}
 
 	return (p);
