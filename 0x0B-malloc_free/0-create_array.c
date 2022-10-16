117559caad84c5b0f29f4ba81bad66324f602d55
Luke Mwangi <lukamwangi.k@gmail.com>
2022-10-16 12:29:18 +0300

    cor 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,6 +11,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
+	int i;
 
 	if (size <= 0)
 	{
@@ -24,7 +25,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	*p = c;
+	for (i = 0; i < size; i++)
+	{
+		*(p + i) = c;
+	}
 
 	return (p);
 }
