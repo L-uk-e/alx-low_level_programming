fa79aad75a4ac385e4b94f3b6b5575fbb46da64c
Luke Mwangi <lukamwangi.k@gmail.com>
2022-10-18 09:00:48 +0300

    Task 0.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,4 @@
-#include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
  * sum_them_all - adds numbers provided as optional arguments
@@ -9,7 +9,7 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
-	unsigned int i = 0;
+	counter i = 0;
 
 	if (n == 0)
 	{
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+typedef unsigned int counter;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
