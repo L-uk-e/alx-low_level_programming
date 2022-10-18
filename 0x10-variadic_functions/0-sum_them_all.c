86ef749e302e1328933adaf854f3445f70ca7c82
Luke Mwangi <lukamwangi.k@gmail.com>
2022-10-18 08:57:12 +0300

    Task 0.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,7 +9,7 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (n == 0)
 	{
@@ -20,7 +20,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(ap, n);
 
-	for (i = 0; i < n; i++)
+	for (; i < n; i++)
 	{
 		sum += va_arg(ap, int);
 	}
