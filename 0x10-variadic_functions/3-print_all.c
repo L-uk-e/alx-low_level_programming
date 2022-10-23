4667402d5db111909eb92875b04a058de953d52c
Luke Mwangi <lukamwangi.k@gmail.com>
2022-10-23 11:26:19 +0300

    Modification 1 Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * separator - separates words
+ * @next: pointer to check for next char
+ *
+ * Return: void
+ */
+void separator(char next)
+{
+	if (next)
+	{
+		printf(", ");
+	}
+}
 
 /**
  * print_all - prints anything
@@ -11,77 +26,41 @@ void print_all(const char * const format, ...)
 {
 	int x = 0;
 	va_list ap;
-	char *separator = ", ";
+	char *word;
 	char character;
 	int number;
 	float dnumber;
-	char *word;
 
 	va_start(ap, format);
 
 	while (*(format + x))
 	{
-		if (*(format + x + 1))
+		switch (*(format + x))
 		{
-			switch (*(format + x))
+		case 'c':
+			character = (char)va_arg(ap, int);
+			printf("%c", character);
+			separator(*(format + x + 1));
+			break;
+		case 'i':
+			number = va_arg(ap, int);
+			printf("%d", number);
+			separator(*(format + x + 1));
+			break;
+		case 'f':
+			dnumber = va_arg(ap, double);
+			printf("%f", dnumber);
+			separator(*(format + x + 1));
+			break;
+		case 's':
+			word = va_arg(ap, char *);
+			if (word == NULL)
 			{
-			case 'c':
-				character = (char)va_arg(ap, int);
-				printf("%c%s", character, separator);
-				break;
-			case 'i':
-				number = va_arg(ap, int);
-				printf("%d%s", number,separator);
-				break;
-			case 'f':
-				dnumber = va_arg(ap, double);
-				printf("%f%s", dnumber, separator);
-				break;
-			case 's':
-				word = va_arg(ap, char *);
-				if (word == NULL)
-				{
-					printf("(nil)");	
-				}
-				else
-				{
-					printf("%s%s", word, separator);
-				}
-				break;
-			default:
-				break;
+				word = "(nil)";
 			}
-		}
-		else
-		{
-			switch (*(format + x))
-			{
-			case 'c':
-				character = (char)va_arg(ap, int);
-				printf("%c", character);
-				break;
-			case 'i':
-				number = va_arg(ap, int);
-				printf("%d", number);
-				break;
-			case 'f':
-				dnumber = va_arg(ap, double);
-				printf("%f", dnumber);
-				break;
-			case 's':
-				word = va_arg(ap, char *);
-				if (word == NULL)
-				{
-					printf("(nil)");	
-				}
-				else
-				{
-					printf("%s", word);
-				}
-				break;
-			default:
-				break;
-			}
+			printf("%s", word);
+			separator(*(format + x + 1));
+			break;
 		}
 		x++;
 	}
