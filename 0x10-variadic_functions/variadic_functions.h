bfacd46852c2bb9fe6a476fac0ef3026cfbc301a
Luke Mwangi <lukamwangi.k@gmail.com>
2022-10-23 07:33:32 +0300

    Made some changes

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,11 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-int _putchar(char c);
+#include <stdio.h>
+#include <stdarg.h>
+
+typedef unsigned int count;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
