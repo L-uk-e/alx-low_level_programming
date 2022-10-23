d77fd0b438e5f1ace3b5a15736cbb248d0ff4759
Luke Mwangi <lukamwangi.k@gmail.com>
2022-10-23 08:06:59 +0300

    Modified 1 Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i < n - 1 && *separator != '\0')
+		if (i < n - 1 && separator)
 		{
 			printf("%d%s", va_arg(ap, int), (char *)separator);
 		}
