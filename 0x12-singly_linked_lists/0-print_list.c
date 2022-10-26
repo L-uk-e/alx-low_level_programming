f6cbc7c9498b23cfcd4851e5349b33281a369273
Luke Mwangi <lukamwangi.k@gmail.com>
2022-10-26 05:29:00 +0300

    Did task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_list - prints all elements of a list
+ * @h: pointer to the first node
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int x;
+	size_t y = 0;
+	char *word;
+	const list_t* temp = (list_t*)malloc(sizeof(list_t));
+
+	temp = h;
+	while (temp != NULL)
+	{
+		word = temp->str;
+		x = 0;
+
+		if (word == NULL)
+		{
+			word = "(nil)";
+		}
+		else
+		{
+			while (*(word + x) != '\0')
+			{
+				x++;
+			}
+		}
+		
+		if (x == 0)
+		{
+			word = "(nil)";
+		}
+
+		printf("[%d] %s\n", x, word);
+
+		temp = temp->next;
+		y++;
+	}
+
+	return (y);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+# Dir for singly linked lists tasks
