f15be9bd07e4c5106243c6032437cf9db57e7b3d
Luke Mwangi <lukamwangi.k@gmail.com>
2022-10-26 10:21:22 +0300

    Did Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list
+ * @head: Address of first element
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t **temp = (list_t **)malloc(sizeof(list_t *));
+
+	while (head != NULL)
+	{
+		*temp = head;
+
+		while (*temp != NULL)
+		{
+			*temp = (*temp)->next;
+		}
+
+		free(*temp);
+	}
+
+	free(temp);
+}
