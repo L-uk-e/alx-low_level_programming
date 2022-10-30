bb109463e787f482b182496936dc0df02cdc1544
Luke Mwangi <lukamwangi.k@gmail.com>
2022-10-30 09:06:08 +0300

    Task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints first 98 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int x = 1, y = 2, next;
+	int count = 0;
+
+	printf("%ld", x);
+
+	while (count < 97)
+	{
+		printf(", %ld", y);
+		next = x + y;
+		x = y;
+		y = next;
+		count ++;
+	}
+	printf("\n");
+
+	return (0);
+}
+
+	
