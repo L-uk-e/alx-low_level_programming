5a2a46c455e406f0176b182eefe7e9c02b86a5b5
Luke Mwangi <lukamwangi.k@gmail.com>
2022-10-30 08:32:06 +0300

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 50 Fibonacci numbers starting with 1 and 2
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int w, x = 2, y = 3, z;
+
+	printf("1, 2\n");
+
+	for (w = 0; w < 50; w++)
+	{
+		printf(", %d", y);
+		z = y;
+		y = y + x;
+		x = z;
+	}
+	printf("\n");
+
+	return (0);
+}
