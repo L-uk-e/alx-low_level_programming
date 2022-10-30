33990e137c6b5a6ca9b0ae6da0b6a03859b30b32
Luke Mwangi <lukamwangi.k@gmail.com>
2022-10-30 08:48:59 +0300

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints sum of even valued terms in the fibonacci sequence
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int next, x = 1, y = 2, sum = 0;
+
+	while (x <= 4000000)
+	{
+		if ((x + y) % 2 == 0)
+		{
+			sum += x;
+		}
+		next = x + y;
+		x = y;
+		y = next;
+	}
+	printf("%ld\n", sum);
+
+	return (0);
+}
