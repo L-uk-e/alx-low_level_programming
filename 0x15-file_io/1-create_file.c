c5929206fade11660c290aa28116f4a74d02b5ae
Luke Mwangi <lukamwangi.k@gmail.com>
2022-11-09 10:44:17 +0300

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ * @filename: name of file to create
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success
+ * -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, count = 0;
+	size_t written;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	while (*(text_content + count) != '\0')
+	{
+		count++;
+	}
+
+	if (text_content != NULL)
+	{
+		written = write(fd, text_content, count);
+		if (written == -1)
+		{
+			return (-1);
+		}
+	}
+
+	close(fd);
+
+	return (1);
+}
