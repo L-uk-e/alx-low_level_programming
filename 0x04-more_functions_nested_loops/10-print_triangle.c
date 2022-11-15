38f8cfccaea72e321b7c9ba2ef5ac86ae5181d19
Luke Mwangi <lukamwangi.k@gmail.com>
2022-11-15 09:33:44 +0300

    Mod 1 Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,13 +11,18 @@ void print_triangle(int size)
 	/* This variable x deals with number of lines*/
 	int x = 0;
 
+	if (x >= size)
+	{
+		_putchar('\n');
+	}
+
 	while (x < size)
 	{
 		/* This variable z deals with number of #*/
 		int z = 0;
 		/* This variable y deals with number of ' '(spaces)*/
 		int y = ((size - 1) - x);
-		
+
 		while (y > 0)
 		{
 			_putchar(' ');
